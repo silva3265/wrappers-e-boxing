5225bdba21e1eeb111b844415f136bc5c7c9df84
silva3265 <silva3265@gmail.com>
2024-03-01 16:14:17 -0300

    Boas práticas: prefira tipos primitivos a wrappers

---
+++ src/Cliente.java
@@ -0,0 +1,10 @@
+
+public class Cliente {
+
+	String nome;
+	Integer idade;
+	Double rendaMensal;
+	int pontosProgramaFidelidade;
+	boolean ativo;
+
+}
---
+++ src/Principal3.java
@@ -0,0 +1,19 @@
+
+public class Principal3 {
+
+	public static void main(String[] args) {
+		long tempoInicio = System.currentTimeMillis();
+
+		// long soma = 0L;
+		Long soma = 0L;
+		for (long i = 0; i < Integer.MAX_VALUE; i++) { //MAX_VALUE - numero maximo
+			soma = soma + i;
+		}
+
+		System.out.println(soma);
+
+		double duracao = (System.currentTimeMillis() - tempoInicio) / 1000d;
+		System.out.printf("Duração: %.2fs%n", duracao);
+	}
+
+}
